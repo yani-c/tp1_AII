b00ee8eed4ee78883287a82fc7ad0476840b3677
yani-c <yaniceli1d@gmail.com>
2019-04-11 09:02:22 -0300

    base del programa

---
+++ StringMatching/StringMatching.java
@@ -0,0 +1,24 @@
+
+public class StringMatching {
+	/**
+	 * Calcula String Matching usando Fuerza Bruta.
+	 * @param text texto a analizar
+	 * @param pattern patron a buscar en el texto
+	 * @returns indice de la primera ocurrencia de pattern en text, en caso de que exista,
+	 * o -1 en caso que no se encuentre. 
+	 */
+	public static int match (String text, String pattern) {
+		return -1; //resolver el problema
+	}
+	
+	/**
+	 * Calcula String Matching usando Knuth-Morris-Pratt.
+	 * @param text texto a analizar
+	 * @param pattern patron a buscar en el texto
+	 * @returns indice de la primera ocurrencia de pattern en el text, en caso que exista,
+	 * o -1 en caso que no se encuentre.
+	 */
+	public static int matchKmp(String text, String pattern) {
+		return -1;
+	}
+}
