3e683a892fa99c51ea48fbdfd09f0a46920ba65f
yani-c <yaniceli1d@gmail.com>
2019-04-16 10:05:02 -0300

    se comenzo el 2)a.Falta revisar el for mas interno y comentar y hacer test

--- LongestRepetition/longestRepetition/LongestRepetition.java
+++ LongestRepetition/longestRepetition/LongestRepetition.java
@@ -1,5 +1,7 @@
 package longestRepetition;
 
+import java.util.ArrayList;
+
 public class LongestRepetition {
 	/**
 	 * Calcula Longest repetition usando Fuerza Bruta.
@@ -7,7 +9,40 @@ public class LongestRepetition {
 	 * @returns subString de mayor longitud que se repite.
 	 */
 	public static String repetition(String text) {
-		return
+		ArrayList<String> arrayOfSubstrings= subStrings(text);
+		int index=0;
+		String maxString="";
+		while(index<arrayOfSubstrings.size()) {
+			if(arrayOfSubstrings.get(index).length() % 2 ==0){
+				String element=arrayOfSubstrings.get(index);
+				String aux=element.substring(element.length()/2, arrayOfSubstrings.size());
+				boolean equals=true;
+				for(int indexAux=0;indexAux<aux.length() && equals;indexAux++) {
+					//EL FOR SE VA DE RANGO
+					if(!(element.charAt(indexAux) == aux.charAt(indexAux))) {
+						equals=false;
+					}
+				}
+				if(equals) {
+					if(aux.length() > maxString.length()) {
+						maxString=aux;
+					}
+				}
+			}
+			index++;
+		}
+		return maxString;
+	}
+	
+	//auxiliar para la resolucion de repetition
+	public static ArrayList<String> subStrings(String string){
+		ArrayList<String> result= new ArrayList<String>();
+		for(int beginIndex=0;beginIndex<string.length();beginIndex++) {
+			for(int endIndex=beginIndex+1;endIndex<=string.length();endIndex++) {
+				result.add(string.substring(beginIndex, endIndex));
+			}
+		}
+		return result;
 	}
 	
 	/**
@@ -16,6 +51,6 @@ public class LongestRepetition {
 	 * @returns subString de mayor longitud que se repite.
 	 */
 	public static String repetitionDc(String text) {
-		return
+		return " ";
 	}
 }
---
+++ LongestRepetition/longestRepetition/LongestRepetitionTest.java
@@ -0,0 +1,20 @@
+package longestRepetition;
+
+
+
+import org.junit.jupiter.api.Test;
+
+class LongestRepetitionTest {
+
+	@Test
+	void testRepetition() {
+		String text="holahola";
+		System.out.println(LongestRepetition.repetition(text));
+	}//se espera que devuelva "que"
+
+	@Test
+	void testRepetitionDc() {
+		System.out.println("yani");
+	}
+
+}
